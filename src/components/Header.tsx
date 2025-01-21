32e6bac9383bee3a2fe8a11c70e345a2e8ec28f0
Oscar André Huerta García <oscarwaii@hotmail.com>
2025-01-21 09:25:03 -0600

    Full width

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+// src/components/Header.tsx
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+const Header: React.FC = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">Azzylvm</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
