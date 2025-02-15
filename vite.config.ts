aa8a1a521d6e7b2e153a4df19fd94bb3bb662436
Oscar André Huerta García <oscarwaii@hotmail.com>
2025-02-15 12:24:04 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
     host: true, // O también puedes especificar tu IP, ej: '192.168.1.10'
     port: 3000, // Opcional, para cambiar el puerto
   },
-  base: "https://PapaDrago.github.io/Azzylvm",
+  base: "https://papadrago.github.io/Azzylvm",
 });
